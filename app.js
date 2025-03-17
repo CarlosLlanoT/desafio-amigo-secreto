f5f84bb4811a37eb5eee2eff4569a49b46ff1b77
CarlosLlanoT <carlosllanotrecans@gmail.com>
2025-03-17 01:06:24 -0300

    reivisón funciones Actualizar Lista y agregarAmigo

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregarAmigo() {
-    let entradaNombre = document.getElementById("amigo");
-    if(entradaNombre == "") {
+function agregarAmigo(){
+    const inputAmigo = document.getElementById(´amigo´);
+    const nombreAmigo = inputAmigo.ariaValueMax.trim();
+    if(nombre == "") {
         alert("Por favor, inserte un nombre.");
         return;
     } 
-    amigos.push(entradaNombre);
-    document.getElementById("amigo").value = "";
-    listaActualizada();
+if(amigos.includes(nombreAmigo)) {
+    alert(´El nombre ${nombreAmigo}´ ya esta en la lista);
+    return;
+}
+    amigos.push(nombreAmigo);
+    inputAmigo.ariaValu = "";
+
+    actualizarLista();
 
 }
 
-function listaActualizada() {
-    let listaAmigos = document.getElementById("listaAmigos");
+function actualizarLista() {
+    const listaAmigos = document.getElementById(´listaAmigos´);
+    listaAmigos.innerHTML = "";
+
     for( let i=0; i<amigos.length; i++){
-        
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
     }
 }
